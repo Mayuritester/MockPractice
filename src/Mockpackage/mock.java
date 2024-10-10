cd6bab5b19d03a5b6e617cd6899e6afe89484bf0
Mayuritester <computer.tester009@gmail.com>
2024-10-10 16:20:12 +0100

    MockPractice

---
+++ src/Mockpackage/mock.java
@@ -0,0 +1,14 @@
+package Mockpackage;
+
+public class mock {
+
+    public static void main(String[] args){
+
+        System.out.println("Mayuri");
+
+        int y = 5;
+                int x = 5;
+                        System.out.println(y*x);
+
+    }
+}
